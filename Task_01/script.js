0624dcd611871e23b7dc9635ab8783402c4a9701
i-am-hariom <154728082+i-am-hariom@users.noreply.github.com>
2025-04-27 23:10:39 +0530

    Add Files

---
+++ Task_01/script.js
@@ -0,0 +1,4 @@
+// Button click event
+document.getElementById('contactButton').addEventListener('click', function() {
+    alert('Thank you for your interest! Please email me at photographer@example.com.');
+});
